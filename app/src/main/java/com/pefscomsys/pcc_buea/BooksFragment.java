1e564f3032e1c3b7d59ca4d35f0e366e09c60dec
Mangielijah <mangielijah8@gmail.com>
2018-08-30 09:45:42 +0100

    hymns view modified

--- app/src/main/java/com/pefscomsys/pcc_buea/BooksFragment.java
+++ app/src/main/java/com/pefscomsys/pcc_buea/BooksFragment.java
@@ -6,24 +6,64 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class BooksFragment extends Fragment {
+public class BooksFragment extends Fragment implements View.OnClickListener {
 
 
     public BooksFragment() {
         // Required empty public constructor
     }
 
+    Button cmfButton, cwfButton, cyfButton, messengerButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_books, container, false);
+        View view = inflater.inflate(R.layout.fragment_books, container, false);
+
+        //Getting button views by their id's in the layout file
+        cmfButton = (Button) view.findViewById(R.id.cmfButton);
+        cwfButton = (Button) view.findViewById(R.id.cwfButton);
+        cyfButton = (Button) view.findViewById(R.id.cyfButton);
+        messengerButton = (Button) view.findViewById(R.id.messengerButton);
+
+        //Setting onClick listeners to button
+        cmfButton.setOnClickListener(this);
+        cwfButton.setOnClickListener(this);
+        cyfButton.setOnClickListener(this);
+        messengerButton.setOnClickListener(this);
+
+
+       return view;
     }
 
+
+
+    //Handles click events for buttons
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+
+            case R.id.cmfButton:
+                Toast.makeText(getActivity(), R.string.cmf_books, Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.cwfButton:
+                Toast.makeText(getActivity(), R.string.cwf_books, Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.cyfButton:
+                Toast.makeText(getActivity(), R.string.cyf_books, Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.messengerButton:
+                Toast.makeText(getActivity(), R.string.messenger, Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+    }
 }
--- app/src/main/java/com/pefscomsys/pcc_buea/MainActivity.java
+++ app/src/main/java/com/pefscomsys/pcc_buea/MainActivity.java
@@ -3,13 +3,25 @@ package com.pefscomsys.pcc_buea;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private FrameLayout mainFrame;
+
+    private HomeFragment homeFragment;
+    private InfoFragment infoFragment;
+    private BooksFragment booksFragment;
+    private HymesFragment hymesFragment;
+    private ScripturesFragment scripturesFragment;
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -22,29 +34,82 @@ public class MainActivity extends AppCompatActivity {
             //switch to each view for a different menu item
             if (id == R.id.navigation_home) {
                 //start home fragment
+                setFragment(homeFragment);
+                Toast.makeText(getApplicationContext(), R.string.home, Toast.LENGTH_SHORT).show();
                 return true;
             } else if (id == R.id.navigation_hymns) {
                 //start hymn fragment
+                setFragment(hymesFragment);
+                Toast.makeText(getApplicationContext(), R.string.hymns, Toast.LENGTH_SHORT).show();
                 return true;
             } else if (id == R.id.navigation_books) {
                 //start book fragment
+                setFragment(booksFragment);
+                Toast.makeText(getApplicationContext(), R.string.books, Toast.LENGTH_SHORT).show();
                 return true;
             } else if (id == R.id.navigation_scriptures) {
                 //start scripture fragment
+                setFragment(scripturesFragment);
+                Toast.makeText(getApplicationContext(), R.string.scriptures, Toast.LENGTH_SHORT).show();
+                return true;
+            } else if (id == R.id.navigation_info) {
+                //start scripture fragment
+                setFragment(infoFragment);
+                Toast.makeText(getApplicationContext(), R.string.church_info, Toast.LENGTH_SHORT).show();
                 return true;
             }
             return false;
         }
-    };
 
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        homeFragment = new HomeFragment();
+        infoFragment = new InfoFragment();
+        booksFragment = new BooksFragment();
+        hymesFragment = new HymesFragment();
+        scripturesFragment = new ScripturesFragment();
+
+        setFragment(homeFragment);
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        mainFrame = (FrameLayout) findViewById(R.id.main_frame);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+
+            case R.id.settings:
+                Toast.makeText(getApplicationContext(), R.string.setting, Toast.LENGTH_SHORT).show();
+                return true;
+            case  R.id.about:
+                Toast.makeText(getApplicationContext(), R.string.about, Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame, fragment);
+        fragmentTransaction.commit();
     }
 
 //    public static String html2text(String html) {
