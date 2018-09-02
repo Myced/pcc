1bc6619784796f27b668cba42127867285a1c2e8
tnced <tncedric@yahoo.com>
2018-09-02 13:26:46 +0100

    Started work on Scriptures

--- app/src/main/java/com/pefscomsys/pcc_buea/ScripturesFragment.java
+++ app/src/main/java/com/pefscomsys/pcc_buea/ScripturesFragment.java
@@ -1,11 +1,13 @@
 package com.pefscomsys.pcc_buea;
 
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
 
 
 /**
@@ -18,12 +20,28 @@ public class ScripturesFragment extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_scriptures, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_scriptures, container, false);
+
+//        mCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+//            @Override
+//            public void onSelectedDayChange(CalendarView CalendarView, int year, int month, int dayOfMonth) {
+//                String date = year + "/" + month + "/"+ dayOfMonth ;
+//                Log.d("Calendar", "onSelectedDayChange: yyyy/mm/dd:" + date);
+//                Intent intent = new Intent(Intent.ACTION_INSERT);
+//                intent.putExtra("date",date);
+//                startActivity(intent);
+//
+//            }
+//        });
+
+
+        return view;
+
     }
 
 }
