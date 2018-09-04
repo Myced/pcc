1e2b67729eaa27f1a279970bde7abf34af537e98
tnced <tncedric@yahoo.com>
2018-09-04 13:38:14 +0100

    Added Spinners on the scripture fragment

--- app/src/main/java/com/pefscomsys/pcc_buea/ScripturesFragment.java
+++ app/src/main/java/com/pefscomsys/pcc_buea/ScripturesFragment.java
@@ -1,13 +1,22 @@
 package com.pefscomsys.pcc_buea;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CalendarView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.SimpleTimeZone;
+import java.util.TimeZone;
+import java.util.Calendar;
 
 
 /**
@@ -15,6 +24,22 @@ import android.widget.CalendarView;
  */
 public class ScripturesFragment extends Fragment {
 
+    //declare private global variables
+    Spinner day;
+    Spinner month;
+    Spinner year;
+    Button findButton;
+
+    String selectedMonth;
+    String selectedDay;
+    String selectedYear;
+
+    String currentYear;
+    String currentMonth;
+    String currentDay;
+
+
+    GregorianCalendar calender;
 
     public ScripturesFragment() {
         // Required empty public constructor
@@ -27,21 +52,174 @@ public class ScripturesFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_scriptures, container, false);
 
-//        mCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-//            @Override
-//            public void onSelectedDayChange(CalendarView CalendarView, int year, int month, int dayOfMonth) {
-//                String date = year + "/" + month + "/"+ dayOfMonth ;
-//                Log.d("Calendar", "onSelectedDayChange: yyyy/mm/dd:" + date);
-//                Intent intent = new Intent(Intent.ACTION_INSERT);
-//                intent.putExtra("date",date);
-//                startActivity(intent);
-//
-//            }
-//        });
+        //we have to set the selected dates to represent the date of today
+
+        //start by initialising the timezone ids
+        String[] ids = TimeZone.getAvailableIDs(+1 * 60 * 60 * 1000);
+
+        if(ids.length == 0)
+        {
+            Toast.makeText(getContext(), "Could not get the time", Toast.LENGTH_SHORT);
+        }
+
+        //prepare the timezone
+        SimpleTimeZone timeZone = new SimpleTimeZone(+1 * 60 * 60 * 1000, ids[0]);
+
+        Calendar calendar = new GregorianCalendar(timeZone);
+
+        Log.d("Calendar", "Printing Date and Time");
+
+        //get the date of today.
+        // the day, month and year
+        Date today = Calendar.getInstance().getTime();
+
+        //now format the date
+        SimpleDateFormat dayFormat = new SimpleDateFormat("dd");
+        SimpleDateFormat monthFormat = new SimpleDateFormat("MM");
+        SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy");
+
+        //now get the current day, month and year
+        currentDay = dayFormat.format(today);
+        currentMonth = monthFormat.format(today);
+        currentYear = yearFormat.format(today);
+
+        Log.d("Date", currentDay);
+        Log.d("Date", currentMonth);
+        Log.d("Date", currentYear);
+
+        String res = "Day:" + currentDay + " Month:" + currentMonth + " Year:" + currentYear;
+
+        Toast.makeText(getContext(), res, Toast.LENGTH_SHORT).show();
+
+
+
+        //initialise the spinners
+        day = view.findViewById(R.id.day_spinner);
+        month = view.findViewById(R.id.month_spinner);
+        year = view.findViewById(R.id.year_spinner);
+        findButton = view.findViewById(R.id.find_btn);
+
+            //initialising the days adapter
+        ArrayAdapter<CharSequence> daysAdapter = ArrayAdapter.createFromResource(getActivity().getApplicationContext(),
+                R.array.days, android.R.layout.simple_spinner_item);
+
+        daysAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        day.setAdapter(daysAdapter);
+
+        //prepare the months adapter too.
+        ArrayAdapter<CharSequence> monthsAdapter = ArrayAdapter.createFromResource(getActivity().getApplicationContext(),
+                R.array.months, android.R.layout.simple_spinner_item);
+
+        daysAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        month.setAdapter(monthsAdapter);
+
+        //year resources id
+
+        ArrayAdapter<CharSequence> yearsAdapter = ArrayAdapter.createFromResource(getActivity().getApplicationContext(),
+                R.array.years, android.R.layout.simple_spinner_item);
+
+        daysAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        year.setAdapter(yearsAdapter);
+
+        //set and onclick listener to the button
+        findButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Filter Date string", Toast.LENGTH_SHORT).show();
+                Log.d("Scripture:", "filter the scriptures");
+
+                //now get the dates selected
+                selectedMonth = (String) month.getSelectedItem();
+                selectedDay = (String) day.getSelectedItem();
+                selectedYear = (String) year.getSelectedItem();
+
+                //get the month code
+                String monthID = getMonthId(selectedMonth);
+
+                String toast = "Month: " + selectedMonth + " Day: " + selectedDay + " Year: " + selectedYear + " MonthId: " + monthID;
+
+                Log.d("Scripture", toast);
+
+                Toast.makeText(getContext(), toast, Toast.LENGTH_SHORT).show();
+
+                //now get the scriptures for this particular day
+
+
+
+            }
+        });
+
 
 
         return view;
 
+    }
+
+    /**
+     * @param month
+     * @return monthID eg 02
+     */
+    private String getMonthId(String month)
+    {
+        String result = "";
+
+        if(month.equals("January"))
+        {
+            result = "01";
+        }
+        else if (month.equals("February"))
+        {
+            result = "02";
+        }
+        else if(month.equals("March"))
+        {
+            result = "03";
+        }
+        else if(month.equals("April"))
+        {
+            result = "04";
+        }
+        else if(month.equals("May"))
+        {
+            result = "05";
+        }
+        else if(month.equals("June"))
+        {
+            result = "06";
+        }
+        else if(month.equals("July"))
+        {
+            result = "07";
+        }
+        else if(month.equals("August"))
+        {
+            result = "08";
+        }
+        else if(month.equals("September"))
+        {
+            result = "09";
+        }
+        else if(month.equals("October"))
+        {
+            result = "10";
+        }
+        else if(month.equals("November"))
+        {
+            result = "11";
+        }
+        else if(month.equals("December"))
+        {
+            result = "12";
+        }
+        else
+        {
+            result = "-00";
+        }
+
+        return result;
     }
 
 }
