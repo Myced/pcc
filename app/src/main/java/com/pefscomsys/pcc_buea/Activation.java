4467ac676ec90c6174b768270c7f4236342b0055
tnced <tncedric@yahoo.com>
2018-09-26 09:17:03 +0100

    Implemented class Activation for check for various modules activation

--- app/src/main/java/com/pefscomsys/pcc_buea/Activation.java
+++ app/src/main/java/com/pefscomsys/pcc_buea/Activation.java
@@ -1,11 +1,15 @@
 package com.pefscomsys.pcc_buea;
 
 import android.content.Context;
+import android.database.Cursor;
+
+import java.io.IOException;
 
 public class Activation
 {
     private Context context;
     public ScriptureDBHandler db;
+    private final int STATUS_INDEX = 2;
 
     public Activation(Context context)
     {
@@ -14,7 +18,71 @@ public class Activation
 
     public boolean checkDiary(String year)
     {
+        //the status is false by default;
+        boolean status = false;
+        String currentStatus = "";
 
-        return false;
+        //try to initialise the database;
+        try {
+            this.db.createDataBase();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //open the datbase
+        this.db.openDataBase();
+
+        //do processing here
+        String query = "SELECT * FROM `diary_payments` WHERE `year` = '" + year + "' ";
+
+        Cursor result = this.db.myDataBase.rawQuery(query, null);
+
+        if(result.getCount() == 0)
+        {
+            status = false;
+        }
+        else
+        {
+            while(result.moveToNext())
+            {
+                currentStatus = result.getString(STATUS_INDEX);
+            }
+
+            //now check if the result is true or false;
+            if(currentStatus.equals("TRUE"))
+            {
+                status = true;
+            }
+            else
+            {
+                status = false;
+            }
+        }
+
+        //close the connection
+        this.db.close();
+
+        return status;
+    }
+
+    public boolean checkHymn()
+    {
+        //TODo write code to check hymn activation
+
+        return true;
+    }
+
+    public boolean checkEcho(String name)
+    {
+        //TODO write code to check if a particular echo had been paid for
+
+        return true;
+    }
+
+    public boolean checkMessenger(String year)
+    {
+        //TODO write code to check messenger activation for a particular year
+
+        return true;
     }
 }
