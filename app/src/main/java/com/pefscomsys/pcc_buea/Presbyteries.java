1d9a3bd0b8d316697798b8c0752abc76cd5dad6b
tnced <tncedric@yahoo.com>
2018-10-05 13:03:59 +0100

    Started work on congregation and presbyteries

---
+++ app/src/main/java/com/pefscomsys/pcc_buea/Presbyteries.java
@@ -0,0 +1,19 @@
+package com.pefscomsys.pcc_buea;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Presbyteries extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_presbyteries);
+
+        setTitle(R.string.congregation);
+
+
+    }
+}
